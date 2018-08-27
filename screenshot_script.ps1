8d39c6aa18861fca477178076fbf2de32acd5b1c
Alban Q <37601993+albanqafa@users.noreply.github.com>
2018-08-27 14:55:23 -0400

    Create screenshot_script.ps1

---
+++ screenshot_script.ps1
@@ -0,0 +1,12 @@
+$File = "c:\WINDOWS\screen.png"
+Add-Type -AssemblyName System.Windows.Forms
+Add-type -AssemblyName System.Drawing
+$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$Width = $Screen.Width
+$Height = $Screen.Height
+$Left = $Screen.Left
+$Top = $Screen.Top
+$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+$bitmap.Save($File)
