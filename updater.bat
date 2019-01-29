9798ee3f4008ed86dadbaa1ecde08349bc73983c
Alban Q <37601993+albanqafa@users.noreply.github.com>
2019-01-29 17:02:14 -0500

    fixed it? needs more testing

--- updater.bat
+++ updater.bat
@@ -1,56 +1,67 @@
 @ECHO OFF
-if exist C:\ProgramData\chocolatey\bin\choco.exe (
+echo Checking for chocolatey...
+WHERE choco.exe
+IF %ERRORLEVEL% NEQ 0 powershell.exe -command "Set-ExecutionPolicy RemoteSigned" && powershell.exe -command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+echo OK
+echo.
 echo Updating 7zip via Chocolatey...
-cup -y 7zip.commandline
+powershell (start-process -filepath cup.exe -argumentlist @('-y','7zip.commandline') -verb runas)
 echo.
 echo Updating sysinternals via Chocolatey...
-cup -y sysinternals
-mklink %cd%\psexec.exe C:\ProgramData\chocolatey\lib\sysinternals\tools\psexec.exe
-mklink %cd%\psinfo.exe C:\ProgramData\chocolatey\lib\sysinternals\tools\psinfo.exe
-mklink %cd%\pskill.exe C:\ProgramData\chocolatey\lib\sysinternals\tools\pskill.exe
-mklink %cd%\psloggedon.exe C:\ProgramData\chocolatey\lib\sysinternals\tools\psloggedon.exe
+powershell (start-process -filepath cup.exe -argumentlist @('-y','sysinternals') -verb runas)
+if exist %cd%\psexec.exe (goto skippsexec)
+	mklink %cd%\psexec.exe %ProgramData%\chocolatey\lib\sysinternals\tools\psexec.exe
+	mklink %cd%\psinfo.exe %ProgramData%\chocolatey\lib\sysinternals\tools\psinfo.exe
+	mklink %cd%\pskill.exe %ProgramData%\chocolatey\lib\sysinternals\tools\pskill.exe
+	mklink %cd%\psloggedon.exe %ProgramData%\chocolatey\lib\sysinternals\tools\psloggedon.exe
+:skippsexec
 echo.
-echo updating CSVfileView via Chocolatey...
-cup -y CSVFileView
-mklink %cd%\CSVFileView.exe C:\ProgramData\chocolatey\lib\csvfileview\tools\csvfileview.exe
+echo Updating CSVfileView via Chocolatey...
+powershell (start-process -filepath cup.exe -argumentlist @('-y','CSVFileView') -verb runas)
+if exist %cd%\CSVFileView.exe (goto skipcsvfileview)
+	mklink %cd%\CSVFileView.exe C:\ProgramData\chocolatey\lib\csvfileview\tools\csvfileview.exe
+:skipcsvfileview
 echo.
-echo updating irfanview via Chocolatey...
-cup -y irfanview
-mkdir %cd%\IrfanViewPortable
-mklink %cd%\IrfanViewPortable\IrfanViewPortable.exe "C:\Program Files\IrfanView\i_view64.exe"
+echo Updating irfanview via Chocolatey...
+powershell (start-process -filepath cup.exe -argumentlist @('-y','irfanview') -verb runas)
+if exist %cd%\IrfanViewPortable (goto skipirfanview)
+	mkdir %cd%\IrfanViewPortable
+	mklink %cd%\IrfanViewPortable\IrfanViewPortable.exe "C:\Program Files\IrfanView\i_view64.exe"
+:skipirfanview
 echo.
 echo Updating ffmpeg...
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-4.0.2-win64-static.zip', 'ffmpeg.zip')"
-rmdir /Q /S ffmpeg\
-7z e ffmpeg.zip -o%cd%\ffmpeg\bin\ -r ffmpeg-*\bin*
-del /Q ffmpeg.zip
-mklink /H %cd%\ffmpeg.exe ffmpeg\bin\ffmpeg.exe
+rem rmdir /Q /S ffmpeg\
+		7z e ffmpeg.zip -o%cd%\ffmpeg\bin\ -r ffmpeg-*\bin*
+		del /Q ffmpeg.zip
+if exist %cd%\ffmpeg.exe (goto skipffmpeg)
+	mklink /H %cd%\ffmpeg.exe ffmpeg\bin\ffmpeg.exe
+:skipffmpeg
 echo.
 echo Updating MonaServer...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://newcontinuum.dl.sourceforge.net/project/monaserver/MonaServer_Win32.zip', 'MonaServer.zip')"
-7z x monaserver.zip -o%cd%\MonaServer\
-del /Q monaserver.zip
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/albanqafa/REMOTE-DUDE/master/Monaserver/www/index.html', '%cd%\Monaserver\www\index.html')"
-.echo
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://newcontinuum.dl.sourceforge.net/project/monaserver/MonaServer_Win32.zip', 'MonaServer.zip')"
+	7z x monaserver.zip -o%cd%\MonaServer\
+	del /Q monaserver.zip
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/albanqafa/REMOTE-DUDE/master/Monaserver/www/index.html', '%cd%\Monaserver\www\index.html')"
 echo.
-echo updating nirsoft utilities...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('http://www.nirsoft.net/utils/nircmd.zip', '%cd%\nircmd.zip')"
-7z x nircmd.zip
+echo.
+echo Updating nirsoft utilities...
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('http://www.nirsoft.net/utils/nircmd.zip', '%cd%\nircmd.zip')"
+	7z x nircmd.zip -aoa
 del /Q nircmd.zip
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.nirsoft.net/utils/bluescreenview.zip', '%cd%\bluescreenview.zip')"
-7z x bluescreenview.zip
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.nirsoft.net/utils/bluescreenview.zip', '%cd%\bluescreenview.zip')"
+	7z x bluescreenview.zip -aoa
 del /Q bluescreenview.zip
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.nirsoft.net/utils/turnedontimesview.zip', '%cd%\turnedontimesview.zip')"
-7z x turnedontimesview.zip
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://www.nirsoft.net/utils/turnedontimesview.zip', '%cd%\turnedontimesview.zip')"
+	7z x turnedontimesview.zip -aoa
 del /Q turnedontimesview.zip
 GOTO skipchocoprompt
-)
 echo This script will only update remotedude and not install or upgrade dependencies without chocolately installed
 echo.
 echo Since you dont have it installed set computername to localhost and head to the 1337 menu to install it
 echo.
 :skipchocoprompt
 echo Updating remotedude...
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/albanqafa/REMOTE-DUDE/master/remotedude.bat', 'remotedude.bat')"
-powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/albanqafa/REMOTE-DUDE/master/screenshot_script.ps1', 'screenshot_script.ps1')"
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/albanqafa/REMOTE-DUDE/master/remotedude.bat', 'remotedude.bat')"
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/albanqafa/REMOTE-DUDE/master/screenshot_script.ps1', 'screenshot_script.ps1')"
 pause
